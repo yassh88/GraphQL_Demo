13df44b9d4934795a9bad8ff92c48945df00cc8f
Yashwant Rathore <yassh88@gmail.com>
2019-09-21 22:41:10 +0530

    Added hard coded GraphQL query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash')
 
-const {GraphQLObjectType, GraphQLString} =graphql;
+
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} =graphql;
+
+//dummy
+var books = [
+  {id: '1', name: 'BOOK1' , genre: 'genre1'},
+  {id: '2', name: 'BOOK2' , genre: 'genre3'},
+  {id: '3', name: 'BOOK3' , genre: 'genre3'},
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -9,4 +18,28 @@ const BookType = new GraphQLObjectType({
     name: {type : GraphQLString},
     genre: {type : GraphQLString},
   })
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields:{
+    book: {
+      type : BookType,
+      args: {id: {type : GraphQLString}},
+      resolve(parent,args){
+        console.log('books',books);
+        return _.find(books,{id: args.id})
+      }
+    }
+  }
+});
+
+
+// book(id: "2"){
+//   name,
+//   genre
+// };
+
+module.exports = new GraphQLSchema({
+  query:RootQuery
+});
